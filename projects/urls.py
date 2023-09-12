5577fefde637806e6fd703c55e3b6c82603d41e5
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-09-12 18:08:37 -0300

    req. 4 ok

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,11 @@
 from rest_framework import routers
 from django.urls import path, include
-from projects.views import ProjectsViewSet, ProfilesViewSet
+from projects.views import ProjectsViewSet, ProfileViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"projects", ProjectsViewSet)
-router.register(r"profiles", ProfilesViewSet)
+router.register(r"profiles", ProfileViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Project, Profile
 from .serializers import ProjectsSerializer, ProfilesSerializer
 
@@ -14,10 +16,21 @@ class ProjectsViewSet(viewsets.ModelViewSet):
         serializer.save()
 
 
-class ProfilesViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfilesSerializer
     # permission_classes = [IsOwner]
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile = get_object_or_404(Profile, pk=kwargs["pk"])
+            return render(request, "profile_detail.html", {"profile": profile})
+        return super().retrieve(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save()
